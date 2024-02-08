8f03a871d860db54c637da387e55985fad03e124
Ibrahim Abuaker <ibrahim.aaker@gmail.com>
2024-02-08 17:14:32 +0100

    big

--- HackerNews/src/components/NewsList.jsx
+++ HackerNews/src/components/NewsList.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-export default function NewsList({data}) {
-  
+
+export default function NewsList({ data }) {
   const getDomainFromUrl = (url) => {
     const parsedUrl = new URL(url);
     return parsedUrl.hostname;
   };
 
-    return (
-<div>
-{data.map((item)=> (
-  
-<div className="newsList" key={item.objectID}>
-          {/* Render each news item here */}
-          <h3><a href={item.url}>{item.title}</a></h3> (<a href={item.url}>{getDomainFromUrl(item.url)} </a>)
-          <p>by: {item.author} | {item.updated_at} | comments: {item.num_comments}      
-          </p>
+  return (
+    <div className="newsListContainer">
+      {data.map((item) => (
+        <div className="newsList" key={item.objectID}>
+          <h3><a href={item.url}>{item.title}</a></h3>
+          <p>(<a href={item.url}>{getDomainFromUrl(item.url)}</a>)</p>
+          <p>by: {item.author} | {item.updated_at} | comments: {item.num_comments}</p>
         </div>
       ))}
-</div>
-
-); 
-};
+    </div>
+  );
+}
